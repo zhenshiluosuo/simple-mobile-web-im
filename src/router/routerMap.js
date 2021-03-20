992c491165db2337e480ef99ef48585bba3c8b3b
Barbarossa <shiningmage@icloud.com>
2021-03-20 15:21:48 +0800

    rem

--- src/router/routerMap.js
+++ src/router/routerMap.js
@@ -1,10 +1,10 @@
-import Collect from "@pages/collect";
+import Collections from "@pages/collections";
 import ErrorPage from "@pages/error";
 import Login from "@pages/login";
 const routerMap = [
-  { path: "/collect", name: "Collect", component: Collect, auth: true },
+  { path: "/collections", name: "Collections", component: Collections, auth: true },
   { path: "/login", name: "Login", component: Login },
-  { path: "/", name: "Collect", component: Collect, auth: true },
+  { path: "/", name: "Collections", component: Collections, auth: true },
   { path: "/404", name: "404", component: ErrorPage },
 ]
 export default routerMap
